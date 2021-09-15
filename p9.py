d01a294070cbf7b90c6cc0cf2885e35cf44cc000
Hemangi3598 <90692254+Hemangi3598@users.noreply.github.com>
2021-09-15 16:46:25 +0530

    Add files via upload

---
+++ p9.py
@@ -0,0 +1,9 @@
+# wapp to add two nos using def function
+
+def add(n1, n2):
+	res = n1+n2
+	print("res = ", res)
+
+n1 = int(input("enter first num "))
+n2 = int(input("enter second num"))
+add(n1, n2)
